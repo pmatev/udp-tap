28ea33ab06c1293736f6192a5d1cc0e1248168fb
Peter Matev <peter.matev@deliveroo.co.uk>
2018-04-03 18:25:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # udp-tap
 Proxy &amp; repeat UDP traffic from a host to N destinations
+
+
+Usage:
+```
+usage: listen.py [-h] [--host HOST] dest [dest ...]
+```
+
+e.g.
+```sh
+python3 listen.py --host 127.0.0.1:8125 127.0.0.1:9999 127.0.0.1:9998
+
+# listening on 127.0.0.1:8125
+# proxying to ['127.0.0.1:9999', '127.0.0.1:9998']
+```
---
+++ load_test.py
@@ -0,0 +1,24 @@
+import socket
+import argparse
+
+
+def split_addr(addr):
+	host, port = addr.split(':')
+	return (host, int(port))
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Load test udp socket')
+	parser.add_argument('target', help='Target address')
+	parser.add_argument('--count', type=int, default=1000, help='Number of requests to send')
+	parser.add_argument('--data', default='testing\n', help='Data string to use as payload')
+
+	args = parser.parse_args()
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	for i in range(args.count):
+		s.sendto("{} - {}".format(i, args.data).encode('utf-8'), split_addr(args.target))
+
+if __name__ == '__main__':
+  main()
