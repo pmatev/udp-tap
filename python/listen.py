768022ce67ceb11f3e032e38cbc28ae59abdd919
Peter Matev <peter.matev@deliveroo.co.uk>
2018-04-04 12:09:13 +0100

    Formatting

--- python/listen.py
+++ python/listen.py
@@ -3,32 +3,30 @@ import argparse
 
 
 def split_addr(addr):
-	host, port = addr.split(':')
-	return (host, int(port))
+    host, port = addr.split(':')
+    return (host, int(port))
 
 def main():
-	parser = argparse.ArgumentParser(description='Proxy & repeat udp traffic from a host to N destinations')
-	parser.add_argument('--host', type=str, default="127.0.0.1:8125", help='Host address & port e.g. 127.0.0.1:8125')
-	parser.add_argument('destinations', metavar="dest", nargs='+', help='Destination addresses')
+    parser = argparse.ArgumentParser(description='Proxy & repeat udp traffic from a host to N destinations')
+    parser.add_argument('--host', type=str, default="127.0.0.1:8125", help='Host address & port e.g. 127.0.0.1:8125')
+    parser.add_argument('destinations', metavar="dest", nargs='+', help='Destination addresses')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	print('listening on {}'.format(args.host))
-	print('proxying to {}'.format(args.destinations))
+    print('listening on {}'.format(args.host))
+    print('proxying to {}'.format(args.destinations))
 
-	listen = split_addr(args.host)
-	destinations = [split_addr(d) for d in args.destinations]
+    listen = split_addr(args.host)
+    destinations = [split_addr(d) for d in args.destinations]
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	s.bind(listen)
+    s.bind(listen)
 
-	while True:
-		data, _ = s.recvfrom(1024)
-		for dest in destinations:
-			s.sendto(data, dest)
+    while True:
+        data, _ = s.recvfrom(1024)
+        for dest in destinations:
+            s.sendto(data, dest)
 
 if __name__ == '__main__':
-	main()
-
-#
+    main()
--- python/load_test.py
+++ python/load_test.py
@@ -3,22 +3,22 @@ import argparse
 
 
 def split_addr(addr):
-	host, port = addr.split(':')
-	return (host, int(port))
+    host, port = addr.split(':')
+    return (host, int(port))
 
 
 def main():
-	parser = argparse.ArgumentParser(description='Load test udp socket')
-	parser.add_argument('target', help='Target address')
-	parser.add_argument('--count', type=int, default=1000, help='Number of requests to send')
-	parser.add_argument('--data', default='testing\n', help='Data string to use as payload')
+    parser = argparse.ArgumentParser(description='Load test udp socket')
+    parser.add_argument('target', help='Target address')
+    parser.add_argument('--count', type=int, default=1000, help='Number of requests to send')
+    parser.add_argument('--data', default='testing\n', help='Data string to use as payload')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	for i in range(args.count):
-		s.sendto("{} - {}".format(i, args.data).encode('utf-8'), split_addr(args.target))
+    for i in range(args.count):
+        s.sendto("{} - {}".format(i, args.data).encode('utf-8'), split_addr(args.target))
 
 if __name__ == '__main__':
   main()
